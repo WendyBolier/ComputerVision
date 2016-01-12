b256257149e3d786d28ced9164d9c13fc81bb09c
WendyBolier <wendybolier@hotmail.com>
2016-01-12 16:49:07 +0100

    jeeeej

--- Prac4/src/main.cpp
+++ Prac4/src/main.cpp
@@ -1,12 +1,15 @@
 #include <cstdlib>
 #include <string>
 #include "FaceDetector.h"
+#include <iostream>
 
 using namespace nl_uu_science_gmt;
 
 int main(
 	int argc, char** argv)
 {
+	cv::Rect rect(76, 94, 98, 98);
+	FaceDetector("data\Training Positive", "data\Training Negative", cv::Size(20, 20), 1, rect, 1000, 2);	
 
 	return EXIT_SUCCESS;
 }

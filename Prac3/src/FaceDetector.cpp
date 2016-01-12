46ed2f3c4f65aa1eda7e02db1d7996ea7a70a85a
WendyBolier <wendybolier@hotmail.com>
2016-01-12 14:20:42 +0100

    FaceDetector.cpp

---
+++ Prac3/src/FaceDetector.cpp
@@ -0,0 +1,13 @@
+/*
+* FaceDetector.cpp
+*
+*  Created on: Jan 12, 2016
+*      Authors: Erik and Wendy
+*/
+
+#include "fa"
+
+namespace nl_uu_science_gmt {
+
+	
+}
---
+++ Prac4/FaceDetector.cpp
@@ -0,0 +1,13 @@
+/*
+* FaceDetector.cpp
+*
+*  Created on : Jan 12, 2016
+*      Authors : Erik and Wendy
+*/
+
+#include "src\FaceDetector.h"
+
+namespace nl_uu_science_gmt {
+
+
+}
